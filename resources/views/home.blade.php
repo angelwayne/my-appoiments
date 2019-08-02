f018f83b31e71290e0ee04f0de9ae2fd8c2c6016
angelwayne <al221311480@gmail.com>
2019-08-02 16:53:36 -0500

    Video    11
    App Kotlin & Laravel 5.8

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                                     </div>
                                 @endif
             
-                                You are logged in!
+                                Ya estas Dentro!
                             </div>
                         </div>
                     </div>
